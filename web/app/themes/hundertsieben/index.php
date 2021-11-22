4739f6e295215adaa629934587ddbd8f9d91e554
Christoph <christoph@ag-prop.com>
2021-11-22 21:55:51 +0100

    Feat(Theme): Add basic theme and install ember

---
+++ web/app/themes/hundertsieben/index.php
@@ -0,0 +1,2 @@
+<?php
+include('dist/index.php');
---
+++ web/app/themes/hundertsieben/intervention.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'application' => [
+        'plugins' => [
+            'advanced-custom-fields-pro/acf.php' => true,
+            'duplicate-post' => true,
+            'enable-media-replace' => true,
+            'wp-api-menus' => true,
+            'wp-migrate-db-pro' => true,
+            'wp-migrate-db-pro-media-files' => true,
+        ],
+    ]
+];
