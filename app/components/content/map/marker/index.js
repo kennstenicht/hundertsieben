9eb2b96e0130973d71b1fc16b6a2967f0a9a13c0
Christoph <christoph@ag-prop.com>
2022-01-10 08:45:58 +0100

    Feat(ContentMapMarker): Style marker and popover

--- app/components/content/map/marker/index.js
+++ app/components/content/map/marker/index.js
@@ -5,8 +5,18 @@ export default class ContentMapMarkerComponent extends Component {
   constructor(owner, args) {
     super(...arguments);
 
-    const marker = new mapboxgl.Marker()
+    let el = document.createElement('div');
+    el.classList.add('c-content-map-marker');
+    el.classList.add(`c-content-map-marker--icon-${args.icon}`);
+
+    const marker = new mapboxgl.Marker(el)
       .setLngLat([args.lng, args.lat])
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+            `<h4 class="u-margin-bottom-none">${args.title}</h4><a href="${args.link}" target="_blank">Zur Webseite</a>`
+          )
+      )
       .addTo(args.map);
   }
 }
