759ead80e17a3d5c56ea8b6310700570f0130a73
Christoph <christoph@ag-prop.com>
2022-01-12 08:45:53 +0100

    Fix(ScrollHint): Add smotthscroll polyfill

--- app/components/application/scroll-hint/index.js
+++ app/components/application/scroll-hint/index.js
@@ -1,15 +1,32 @@
 
 
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import smoothscroll from 'smoothscroll-polyfill';
 
 export default class ApplicationScrollHintComponent extends Component {
+  // Services
+  @service fastboot;
+
+
+  constructor() {
+    super(...arguments);
+
+    if (!this.fastboot.isFastBoot) {
+      // kick off the polyfill!
+      smoothscroll.polyfill();
+    }
+  }
+
   // Actions
   @action
   scrollDown() {
-    let scroller = document.querySelector('.c-application__content');
-    let newScrollPosition = window.innerHeight + scroller.scrollTop;
+    if (!this.fastboot.isFastBoot) {
+      let scroller = document.querySelector('.c-application__content');
+      let newScrollPosition = window.innerHeight + scroller.scrollTop;
 
-    scroller.scroll({ top: newScrollPosition, behavior: 'smooth' });
+      scroller.scroll({ top: newScrollPosition, behavior: 'smooth' });
+    }
   }
 }
